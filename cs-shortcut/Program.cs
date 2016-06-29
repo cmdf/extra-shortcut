c9a7159dc335ecfb27da8d23e76e16d65813c696
Subhajit Sahu <wolfram77@gmail.com>
2016-06-29 09:41:35 +0530

    project: created

---
+++ cs-shortcut/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_shortcut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
